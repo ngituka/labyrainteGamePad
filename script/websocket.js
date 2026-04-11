c86004b7fc4f8d772a11c759e49c1bdbc009f9b6
Hardvelt <bbraconnierdamien@gmail.com>
2026-04-11 14:21:30 +0200

    Update websocket.js
    export la socket

--- script/websocket.js
+++ script/websocket.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("wss://127.0.0.1:9082");
+export const socket = new WebSocket("wss://127.0.0.1:9082");
 
 socket.addEventListener("open", (event) => {
   socket.send("Coucou le serveur !");
