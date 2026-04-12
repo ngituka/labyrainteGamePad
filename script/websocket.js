d289389372e2c291a92b6bb3dab58fe47ad90227
Damien Braconnier <damien.braconnier@universite-paris-saclay.fr>
2026-04-13 00:43:17 +0200

    connection faite

--- script/websocket.js
+++ script/websocket.js
@@ -1,38 +1,43 @@
-export var url = ""
-const wssUrl = "wss://" + url + ":9082/";
+var url = window.location.host;
+const ip = url.split(":")[0];
+alert(ip)
+
+const wsUrl = "ws://" + ip + ":9082/";
 export let ws = null;
 let counter = 0;
 let pingInterval;
 
+startConnection();
+
 function initializeWebSocketListeners() {
-  ws.addEventListener("open", () => {
-    console.log("CONNECTED");
+    ws.addEventListener("open", () => {
+        console.log("CONNECTED");
 
-    ws.send("hello");
+        ws.send("hello");
 
-    pingInterval = setInterval(() => {
-      console.log(`SENT: ping: ${counter}`);
-      ws.send("ping");
-    }, 1000);
-  });
+        pingInterval = setInterval(() => {
+            console.log(`SENT: ping: ${counter}`);
+            ws.send("ping");
+        }, 1000);
+    });
 
-  ws.addEventListener("close", () => {
-    console.log("DISCONNECTED");
-    clearInterval(pingInterval);
-  });
+    ws.addEventListener("close", () => {
+        console.log("DISCONNECTED");
+        clearInterval(pingInterval);
+    });
 
-  ws.addEventListener("message", (e) => {
-    console.log(`RECEIVED: ${e.data}: ${counter}`);
-    counter++;
-  });
+    ws.addEventListener("message", (e) => {
+        console.log(`RECEIVED: ${e.data}: ${counter}`);
+        counter++;
+    });
 
-  ws.addEventListener("error", (e) => {
-    console.log("ERROR", e);
-  });
+    ws.addEventListener("error", (e) => {
+        console.log("ERROR", e);
+    });
 }
 
-export function connect(){
-  console.log("OPENING");
-  ws = new WebSocket(wssUrl);
-  initializeWebSocketListeners();
+function startConnection(){
+    console.log("OPENING");
+    ws = new WebSocket(wsUrl);
+    initializeWebSocketListeners();
 }
